d7cb61cf0dbcb0afbe6591465ed2326abc9b0fc9
Berkan Aslan <berkan.aslan@patikaglobal.com>
2024-11-02 14:31:18 +0300

    Initial commit

---
+++ src/test/java/io/berkan/springmodulith/SpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package io.berkan.springmodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
